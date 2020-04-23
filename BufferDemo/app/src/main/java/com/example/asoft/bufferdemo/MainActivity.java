85f02f985f0ed2ba56b0589b52c4446652499b99
Jegan Kabilan <jegancbk@gmail.com>
2020-04-23 12:58:27 -0500

    Buffer more

--- BufferDemo/app/src/main/java/com/example/asoft/bufferdemo/MainActivity.java
+++ BufferDemo/app/src/main/java/com/example/asoft/bufferdemo/MainActivity.java
@@ -2,9 +2,14 @@ package com.example.asoft.bufferdemo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.util.List;
 
 import io.reactivex.Observable;
+import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,7 +25,33 @@ public class MainActivity extends AppCompatActivity {
         Observable<Integer> myObservable = Observable.range(1, 20);
 
         myObservable.subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread());
+                .observeOn(AndroidSchedulers.mainThread())
+                .buffer(4)
+                .subscribe(new Observer<List<Integer>>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(List<Integer> integers) {
+                        Log.i(TAG, "onNext called");
+                        for (Integer i: integers) {
+                            Log.i(TAG, "int value is " + i);
+                        }
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
 
 
 
