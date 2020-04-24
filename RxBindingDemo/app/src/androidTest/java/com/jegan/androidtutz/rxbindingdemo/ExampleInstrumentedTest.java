059b4371fe43b6b40b0bfb22c9ef9b691e6e2aad
Jegan Kabilan <jegancbk@gmail.com>
2020-04-24 15:12:23 -0500

    RxBindings impl

--- RxBindingDemo/app/src/androidTest/java/com/jegan/androidtutz/rxbindingdemo/ExampleInstrumentedTest.java
+++ RxBindingDemo/app/src/androidTest/java/com/jegan/androidtutz/rxbindingdemo/ExampleInstrumentedTest.java
@@ -1,13 +1,14 @@
 package com.jegan.androidtutz.rxbindingdemo;
 
 import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import androidx.test.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -21,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.anushka.androidtutz.rxbindingdemo", appContext.getPackageName());
+        assertEquals("com.jegan.androidtutz.rxbindingdemo", appContext.getPackageName());
     }
 }
--- RxBindingDemo/app/src/main/java/com/jegan/androidtutz/rxbindingdemo/MainActivity.java
+++ RxBindingDemo/app/src/main/java/com/jegan/androidtutz/rxbindingdemo/MainActivity.java
@@ -5,7 +5,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.jakewharton.rxbinding3.view.RxView;
+import com.jakewharton.rxbinding3.widget.RxTextView;
+
 import androidx.appcompat.app.AppCompatActivity;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+import kotlin.Unit;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -22,6 +28,49 @@ public class MainActivity extends AppCompatActivity {
         inputText = findViewById(R.id.etInputField);
         viewText = findViewById(R.id.tvInput);
         clearButton = findViewById(R.id.btnClear);
+
+        /*inputText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                viewText.setText(s);
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                inputText.setText("");
+                viewText.setText("");
+            }
+        });*/
+
+        Disposable disposable = RxTextView.textChanges(inputText)
+                .subscribe(new Consumer<CharSequence>() {
+                    @Override
+                    public void accept(CharSequence charSequence) throws Exception {
+                        viewText.setText(charSequence);
+                    }
+                });
+
+        Disposable disposable2 = RxView.clicks(clearButton)
+                .subscribe(new Consumer<Unit>() {
+                    @Override
+                    public void accept(Unit unit) throws Exception {
+                        inputText.setText("");
+                        viewText.setText("");
+                    }
+                });
 
     }
 }
