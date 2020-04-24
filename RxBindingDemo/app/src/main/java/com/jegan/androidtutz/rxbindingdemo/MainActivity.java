ebdb7445b8c1e79cb7ad79ca3b988c4b296b89fc
Jegan Kabilan <jegancbk@gmail.com>
2020-04-24 13:47:46 -0500

    RxBindings demo

---
+++ RxBindingDemo/app/src/main/java/com/jegan/androidtutz/rxbindingdemo/MainActivity.java
@@ -0,0 +1,27 @@
+package com.jegan.androidtutz.rxbindingdemo;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText inputText;
+    private TextView viewText;
+    private Button clearButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        inputText = findViewById(R.id.etInputField);
+        viewText = findViewById(R.id.tvInput);
+        clearButton = findViewById(R.id.btnClear);
+
+    }
+}
