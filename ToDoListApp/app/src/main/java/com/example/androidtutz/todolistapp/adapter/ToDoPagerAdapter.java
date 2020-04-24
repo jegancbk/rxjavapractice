f66d1822f557f0d55b6d11987b84085acc0acc7a
Jegan Kabilan <jegancbk@gmail.com>
2020-04-24 16:20:04 -0500

    case study

---
+++ ToDoListApp/app/src/main/java/com/example/androidtutz/todolistapp/adapter/ToDoPagerAdapter.java
@@ -0,0 +1,44 @@
+package com.example.androidtutz.todolistapp.adapter;
+
+
+import com.example.androidtutz.todolistapp.TodolistFragment;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+
+/**
+ * Created by K. A. ANUSHKA MADUSANKA on 4/12/2017.
+ */
+
+public class ToDoPagerAdapter extends FragmentStatePagerAdapter {
+    int mNumOfTabs;
+
+    public ToDoPagerAdapter(FragmentManager fm, int NumOfTabs) {
+        super(fm);
+        this.mNumOfTabs = NumOfTabs;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        //returns to do list views
+        switch (position) {
+            case 0:
+
+                TodolistFragment todolistFragment1=TodolistFragment.newInstance("0","to do tasks");
+                return todolistFragment1;
+            case 1:
+
+                TodolistFragment todolistFragment2=TodolistFragment.newInstance("1","done tasks");
+                return todolistFragment2;
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return mNumOfTabs;
+    }
+}
